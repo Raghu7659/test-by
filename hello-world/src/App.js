7cde87ad2d7b366b6d8e4afb5e61bf03639d1b7b
TalluriRaghavendra <talluriraghavendra2@gmail.com>
2025-06-28 09:06:47 +0530

    intial commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,24 @@
+
+
+
+import React from 'react';
+import './App.css'
+let a ="Apple"
+let b = "Banana"
+function App() {
+  return (
+    <div className = "container">
+      <p>I like {a}, {b}</p>
+      <h2>This is Raghu</h2>
+      <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg" alt="Raghu" />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+  
+
+
